6b2f4c46222189479669d881092110fb5ad01df3
Alexander Ananchenko <snvtr@mail.ru>
2023-02-01 11:03:31 +0600

    scripts for k8s

--- db/boot.sh
+++ db/boot.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 export FLASK_APP=db.py
-export LOGR_HOST=$(ip -br a | grep UP | awk '{print $3}' | sed -r 's/\/\w+//')
+# for docker:
+# export LOGR_HOST=$(ip -br a | grep UP | awk '{print $3}' | sed -r 's/\/\w+//')
+# for k8s:
+export LOGR_HOST=logr-service
 flask run --host=0.0.0.0 --port=7000
