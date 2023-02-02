f25f3baa7a7364b600c5ee364cbbf02ade7e913f
Alexander Ananchenko <snvtr@mail.ru>
2023-02-02 10:52:13 +0600

    front-ingres.yaml

--- expose.sh
+++
@@ -1,6 +0,0 @@
-#kubectl expose deployment logr-deployment --type=NodePort --name=logr-service
-#kubectl expose deployment front-deployment --type=NodePort --name=front-service
-
-kubectl expose deployment back-deployment --type=NodePort --name=back-service
-kubectl expose deployment db-deployment --type=NodePort --name=db-service
-
